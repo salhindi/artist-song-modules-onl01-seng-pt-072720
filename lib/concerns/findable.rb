ff95c00a3f61e7b7f29cb9babdb67ee2958e00e8
Sara A <calisaraalhindi@gmail.com>
2020-09-03 06:34:13 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -5,12 +5,12 @@ module Findable
  
   def initialize(name)
     @name = name
-    all << self
+    # all << self
   end
  
-  def all
-    @@all
-  end
+  # def all
+  #   @@all
+  # end
 
   def find_by_name(name)
     self.all.detect {|a| a.name == name}
